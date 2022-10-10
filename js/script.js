8dcc1140135a729bd51e3b44843af4c355600e64
SekoVper <damehlivingstone1@gmail.com>
2022-10-10 12:50:10 +0100

    Add js functionality to menu items.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 const toggler = document.querySelector(".toggler");
 const navMenu = document.querySelector(".menu-items");
+const linkItem = document.querySelectorAll(".menu-links");
 
 toggler.addEventListener("click", () => {
   toggler.classList.toggle("active");
-  navMenu.classList.toggle("active");
+  navMenu.classList.toggle("show-menu");
 })
+
+for(let link of linkItem){
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  })
+}
