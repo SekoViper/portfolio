31e1796014736d86d7b0467079554c8b31ec4eec
SekoVper <damehlivingstone1@gmail.com>
2022-10-11 18:34:03 +0100

    Fix linters

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const toggler = document.querySelector('.toggler');
 const navMenu = document.querySelector('.menu-items');
 const linkItems = document.querySelectorAll('.menu-links');
 
-
 toggler.addEventListener('click', () => {
   toggler.classList.toggle('active');
   navMenu.classList.toggle('show-menu');
@@ -15,84 +14,82 @@ for (let i = 0; i < linkItems.length; i += 1) {
   });
 }
 
-let projects = [
+const projects = [
   {
-    id: 0, 
-    name: "Tonic", 
-    subTitle: {title: 'Canopy', role: 'Back End Dev', year: '2015'},
-    shortDesc: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.", 
-    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ", 
-    imgURL: "img/tonic_desktop.png", 
-    modalURL:"img/modal_Portfolio.png",
+    id: 0,
+    name: 'Tonic',
+    subTitle: { title: 'Canopy', role: 'Back End Dev', year: '2015' },
+    shortDesc: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+    imgURL: 'img/tonic_desktop.png',
+    modalURL: 'img/modal_Portfolio.png',
     techs: [
-      'Html', 'Css', 'Javascript'
-  ], 
-    linkToLive: "https://sekoviper.github.io/portfolio/", 
-    linkToSrc: "https://github.com/SekoViper/portfolio", 
-  },
-  
-  {
-    id: 1, 
-    name: "Multi-Post Stories", 
-    subTitle: {title: 'Canopy', role: 'Back End Dev', year: '2015'}, 
-    shortDesc: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.", 
-    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ", 
-    imgURL: "img/gym%20fit.png", 
-    modalURL:"img/modal_Portfolio.png",
-    techs: [
-      'Html', 'Css', 'Javascript'
-  ], 
-    linkToLive: "https://sekoviper.github.io/portfolio/", 
-    linkToSrc: "https://github.com/SekoViper/portfolio", 
+      'Html', 'Css', 'Javascript',
+    ],
+    linkToLive: 'https://sekoviper.github.io/portfolio/',
+    linkToSrc: 'https://github.com/SekoViper/portfolio',
   },
 
   {
-    id: 2, 
-    name: "SPiiKA French", 
-    subTitle: {title: 'Canopy', role: 'Back End Dev', year: '2015'}, 
-    shortDesc: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.", 
-    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ", 
-    imgURL: "img/Avialability.png", 
-    modalURL:"img/modal_Portfolio.png",
+    id: 1,
+    name: 'Multi-Post Stories',
+    subTitle: { title: 'Canopy', role: 'Back End Dev', year: '2015' },
+    shortDesc: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+    imgURL: 'img/gym%20fit.png',
+    modalURL: 'img/modal_Portfolio.png',
     techs: [
-      'Html', 'Css', 'Javascript'
-  ], 
-    linkToLive: "https://sekoviper.github.io/portfolio/", 
-    linkToSrc: "https://github.com/SekoViper/portfolio", 
+      'Html', 'Css', 'Javascript',
+    ],
+    linkToLive: 'https://sekoviper.github.io/portfolio/',
+    linkToSrc: 'https://github.com/SekoViper/portfolio',
   },
 
   {
-    id: 3, 
-    name: "Professional Art", 
-    subTitle: {title: 'Canopy', role: 'Back End Dev', year: '2015'}, 
-    shortDesc: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.", 
-    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ", 
-    imgURL: "img/art_desktop.png", 
-    modalURL:"img/modal_Portfolio.png",
-    techs: ['Html', 'Css', 'Javascript','Ruby'], 
-    linkToLive: "https://sekoviper.github.io/portfolio/", 
-    linkToSrc: "https://github.com/SekoViper/portfolio", 
+    id: 2,
+    name: 'SPiiKA French',
+    subTitle: { title: 'Canopy', role: 'Back End Dev', year: '2015' },
+    shortDesc: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+    imgURL: 'img/Avialability.png',
+    modalURL: 'img/modal_Portfolio.png',
+    techs: [
+      'Html', 'Css', 'Javascript',
+    ],
+    linkToLive: 'https://sekoviper.github.io/portfolio/',
+    linkToSrc: 'https://github.com/SekoViper/portfolio',
   },
-]
 
-const container = document.getElementById("portfolio")
+  {
+    id: 3,
+    name: 'Professional Art',
+    subTitle: { title: 'Canopy', role: 'Back End Dev', year: '2015' },
+    shortDesc: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+    imgURL: 'img/art_desktop.png',
+    modalURL: 'img/modal_Portfolio.png',
+    techs: ['Html', 'Css', 'Javascript', 'Ruby'],
+    linkToLive: 'https://sekoviper.github.io/portfolio/',
+    linkToSrc: 'https://github.com/SekoViper/portfolio',
+  },
+];
+
+const container = document.getElementById('portfolio');
 
 projects.forEach((project, index) => {
-  let ulElement = document.createElement('ul');
+  const ulElement = document.createElement('ul');
   ulElement.classList.add('techs');
-  project.techs.forEach((item) =>{
-    let liElement = document.createElement('li');
+  project.techs.forEach((item) => {
+    const liElement = document.createElement('li');
     liElement.classList.add('btn', 'btn-secondary');
     liElement.innerHTML = `${item}`;
     ulElement.appendChild(liElement);
-  })
+  });
   const content = `
   <div data-id="${index}" class="work-container">
           <div class="work-image">
             <img
               src="${project.imgURL}"
-              width="278px"
-              height="220px"
               alt="Tonic image"
               class="img-hover-effect"
             />
@@ -118,32 +115,25 @@ projects.forEach((project, index) => {
         </div>
   `;
   container.innerHTML += content;
-  let ulOrigin = document.getElementById(`ul-${index}`);
+  const ulOrigin = document.getElementById(`ul-${index}`);
   ulOrigin.replaceWith(ulElement);
-})
+});
 
 // making work buttons open modal
-const workButtonList = document.querySelectorAll('.work-button')
+const workButtonList = document.querySelectorAll('.work-button');
 
-workButtonList.forEach((button)=>{
-  button.addEventListener('click',()=>{
-    let projectIndex = button.dataset.id;
-    openModal(projectIndex)
-  })
-})
+const modalParent = document.getElementById('modal-parent');
+function openModal(projectIndex) {
+  const project = projects[projectIndex];
 
-const modalParent = document.getElementById('modal-parent')
-function openModal(projectIndex){
-  let project = projects[projectIndex];
- 
-  let ulElement = document.createElement('ul');
+  const ulElement = document.createElement('ul');
   ulElement.classList.add('techs');
-  project.techs.forEach((item) =>{
-    let liElement = document.createElement('li');
+  project.techs.forEach((item) => {
+    const liElement = document.createElement('li');
     liElement.classList.add('btn', 'btn-secondary');
     liElement.innerHTML = `${item}`;
     ulElement.appendChild(liElement);
-  })
+  });
   const content = `<div class="modal-container">
   <div class="modal-header">
     <div class="modal-title">
@@ -179,21 +169,22 @@ function openModal(projectIndex){
     <a class="btn" target="_blank" href="${project.linkToSrc}">See Source <img src="img/github_btn_icon.svg" alt="github"></a>
     </div>
   </div>
-</div>`
+</div>`;
 
-modalParent.innerHTML = content;
-let ulOrigin = document.getElementById(`tech-ul`);
-ulOrigin.replaceWith(ulElement);
+  modalParent.innerHTML = content;
+  const ulOrigin = document.getElementById('tech-ul');
+  ulOrigin.replaceWith(ulElement);
 
-const closeModalButton = document.getElementById('modal-close');
+  const closeModalButton = document.getElementById('modal-close');
 
-closeModalButton.addEventListener("click",()=>{
-  console.log("close button pressed")
-  modalParent.innerHTML="";
-})
-
+  closeModalButton.addEventListener('click', () => {
+    modalParent.innerHTML = '';
+  });
 }
 
-
-
-
+workButtonList.forEach((button) => {
+  button.addEventListener('click', () => {
+    const projectIndex = button.dataset.id;
+    openModal(projectIndex);
+  });
+});
